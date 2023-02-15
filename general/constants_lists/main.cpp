a4b77b427138606930a0d9719bd0d37af8ca8b65
rockwatch <rocksistor@gmail.com>
2023-02-15 09:57:07 -0400

    [PUT] Modified constants_list files for new changes

--- general/constants_lists/main.cpp
+++ general/constants_lists/main.cpp
@@ -4,26 +4,20 @@ using namespace std;
 
 int main() {
     int age = 0;
-    // <data-type> <variable-name> = <value> ;
+    age = 39;
+    //this is how we declare and reassign variables
 
-    age = 34;
-    //variables can be reassigned
+    const char letter = 'A';
+    //declaring and assigning constants
 
-    const char letter = 'b';
-    // 'const' indicates it's a constant making it impossible to alter it in the future
-
-    // letter = 'a'; ← this line throws an error
-
-    int ages_list[] = {14, 83, 51};
-    //declaring a list with <variable_name>[] = {...}
-
-    ages_list[1] = 26;
-    //also variables in lists can be reassigned
+    int ages_list[] = { 23, 61, 17 };
+    //declaring a variable list
 
     cout << age << endl;
-    //'endl' applies a line jump
     cout << letter << endl;
-
     cout << ages_list[1];
-    //to access a variable element in a list we write the index in []
+    //endl makes us a line jump
+
+
+    return 0;
 }
